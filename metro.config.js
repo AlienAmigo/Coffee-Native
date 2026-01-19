575168df1b09f9c2b54d8e2952048d4649209af6
Peter Ryzhov <ryzhov.p@osmocode.ru>
2026-01-19 13:12:53 +0300

    aliases added

---
+++ metro.config.js
@@ -0,0 +1,33 @@
+const {getDefaultConfig} = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+// Добавляем поддержку алиасов
+config.resolver = {
+    ...config.resolver,
+    alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@config': path.resolve(__dirname, 'src/config'),
+        '@constants': path.resolve(__dirname, 'src/constants'),
+        '@database': path.resolve(__dirname, 'src/database'),
+        '@helpers': path.resolve(__dirname, 'src/helpers'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@navigation': path.resolve(__dirname, 'src/navigation'),
+        '@screens': path.resolve(__dirname, 'src/screens'),
+        '@services': path.resolve(__dirname, 'src/services'),
+        '@store': path.resolve(__dirname, 'src/store'),
+        '@types': path.resolve(__dirname, 'src/types'),
+        '@ui': path.resolve(__dirname, 'src/components/ui'),
+    },
+    extraNodeModules: {
+        ...config.resolver.extraNodeModules,
+    },
+    // Добавьте эти расширения
+    sourceExts: ['js', 'jsx', 'ts', 'tsx', 'json', 'cjs'],
+    assetExts: ['png', 'jpg', 'jpeg', 'gif', 'svg', 'ttf', 'otf', 'woff', 'woff2'],
+};
+
+module.exports = config;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,22 +3,15 @@ import {
   MD3Theme,
   useTheme as useRNPTheme,
 } from 'react-native-paper';
-import { Colors } from '@constants/colors';
 
 export interface IReactNativePaperCustomTheme extends MD3Theme {
-  colors: MD3Theme['colors'] & (typeof Colors)['additional'];
+  colors: MD3Theme['colors'];
 }
 
 export const theme: IReactNativePaperCustomTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: Colors.primary,
-    background: Colors.white,
-    surface: Colors.white,
-    onSurfaceVariant: Colors.onSurfaceVariant,
-    // дополнительные цвета
-    ...Colors.additional,
   },
   roundness: 20,
 };
